40c58555feb29a6ea25c6193dc7a762216add5d9
HimanshAhuja <67634795+HimanshAhuja@users.noreply.github.com>
2020-08-09 17:37:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const homeButton = document.getElementById("home")
+const featureButton = document.getElementById("features")
+const buynowButton = document.getElementById("buy")
+const aboutButton = document.getElementById("about")
+
+homeButton.onclick =
+
+    featureButton.onclick =
+
+    buynowButton.onclick =
+
+    aboutButton.onclick =
